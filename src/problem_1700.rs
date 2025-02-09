28d5b648ef52273b0d2b15d9849fef27618f379c
Alex Harris <PersonalXela31@protonmail.com>
2025-02-09 15:19:56 +0000

    Problem 1700: Number of Students Unable to Eat Lunch

---
+++ src/problem_1700.rs
@@ -0,0 +1,30 @@
+use std::collections::VecDeque;
+
+// Just modelled the question very directly using a VecDeck
+impl Solution {
+    pub fn count_students(students: Vec<i32>, sandwiches: Vec<i32>) -> i32 {
+        if (students.len() == 0) || (sandwiches.len()) == 0 {
+            return 0;
+        }
+
+        let mut queue = VecDeque::from(students);
+        let mut num_students = (&queue).len() as i32;
+        for s in sandwiches.iter() {
+            let mut counter = 0;
+            while (Some(s) != queue.front()) {
+                if let Some(sad_student) = queue.pop_front() {
+                    queue.push_back(sad_student);
+                    counter += 1;
+                    if counter == num_students {
+                        return num_students;
+                    }
+                } else {
+                    return 0;
+                }
+            }
+            queue.pop_front();
+            num_students -= 1;
+        }
+        num_students
+    }
+}
