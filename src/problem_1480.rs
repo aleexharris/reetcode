e1dfd3b575085b7df41a7fb0a088cb25a663b71d
Alex Harris <PersonalXela31@protonmail.com>
2025-02-19 08:14:33 +0000

    Problem 1480: Improved initial solution from O(n^2) to O(n)

--- src/problem_1480.rs
+++ src/problem_1480.rs
@@ -1,5 +1,8 @@
 impl Solution {
     pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
-        nums.iter().enumerate().map(|(i, _)| nums[..=i].iter().sum()).collect()
+        nums.iter().scan(0, |acc, &n| {
+            *acc += n;
+            Some(*acc)
+        }).collect()
     }
 }
